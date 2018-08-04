297a5ca67e670d26844e87e91da0f583789a66a0
Ian Price <ianpriceve@gmail.com>
2018-08-03 23:29:14 -0600

    twitch embed resizing and centering

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,15 @@
+const width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+const height = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+const headerHeight = Math.max(document.getElementById('header').clientHeight || 0);
+const useableHeight = height - headerHeight;
+const useableWidth = width-18;
+console.log('width ' + width)
+console.log('height ' + height)
+console.log('header height ' + headerHeight)
+
 new Twitch.Embed("twitch-embed", {
-    width: 940,
-    height: 480,
+    width: useableWidth,
+    height: useableHeight,
     theme: 'dark',
     channel: "the_geeze"
   });
