a37425f92284e9a185f9d2a5a43a4877f9482041
Ian Price <ianpriceve@gmail.com>
2018-08-07 16:25:29 -0600

    dynamic twitch embed sizing

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,16 +1,37 @@
-const width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-const height = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-const headerHeight = Math.max(document.getElementById('header').clientHeight || 0);
-const useableHeight = height - headerHeight;
-const useableWidth = width-18;
+let width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+let height = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+let headerHeight = Math.max(document.getElementById('header').clientHeight || 0);
+let useableHeight = height - headerHeight;
+let useableWidth = width-18;
 
 new Twitch.Embed("twitch-embed", {
+  width: useableWidth,
+  height: useableHeight,
+  theme: 'dark',
+  channel: "the_geeze"
+});
+
+
+function twitchSize() {
+  document.getElementById('twitch-embed').innerHTML = "";
+
+  let width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+  let height = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+  let headerHeight = Math.max(document.getElementById('header').clientHeight || 0);
+  let useableHeight = height - headerHeight;
+  let useableWidth = width-18;
+
+  console.log('WIDTH'+width)
+  console.log('HEIGHT'+height)
+  console.log('HEADER'+headerHeight)
+  console.log('UHEIGHT'+useableHeight)
+  console.log('WIDTH'+useableWidth)
+
+  new Twitch.Embed("twitch-embed", {
     width: useableWidth,
     height: useableHeight,
     theme: 'dark',
     channel: "the_geeze"
   });
-
-
-
+}
 
